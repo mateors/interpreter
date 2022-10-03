a1b56442971efa1ae78d8705dc2be5332ea77f1d
Mostain Billah <bill.rassel@gmail.com>
2022-10-03 18:27:23 +0600

    InfixExpression type and test added

--- ast/ast.go
+++ ast/ast.go
@@ -163,3 +163,29 @@ func (pe *PrefixExpression) TokenLiteral() string {
 func (pe *PrefixExpression) expressionNode() {
 
 }
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+	out.WriteString(";")
+	return out.String()
+}
+
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *InfixExpression) expressionNode() {
+
+}
